a8db441f029958b0db4d0a21aadf8bd2545e048e
pierrolabo <pierrolabo@gmail.com>
2020-05-30 18:37:35 +0200

    init + ticket routes

---
+++ models/Ticket.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//  Create Schema
+const TicketSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  answers: {
+    type: Array,
+    default: [],
+  },
+  history: {
+    type: Array,
+    default: [],
+  },
+  status: {
+    type: String,
+    default: 'New',
+  },
+  assigned_to: {
+    type: Array,
+    default: [],
+  },
+  created_by: {
+    type: String,
+    required: true,
+  },
+  completed_by: {
+    type: String,
+    default: null,
+  },
+  last_updated: {
+    type: Date,
+    default: Date.now,
+  },
+  ip_address: {
+    type: String,
+    default: null,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Ticket = mongoose.model('ticket', TicketSchema);
