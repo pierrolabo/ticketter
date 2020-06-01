ea0d8428cd992f8fe4919c5f0aba6df8eedd6681
pierrolabo <pierrolabo@gmail.com>
2020-06-01 23:39:33 +0200

    BACKEND protected routes ok

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//  Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  register_date: {
+    type: Date,
+    default: Date.now,
+  },
+  address: {
+    type: Array,
+    default: null,
+  },
+  role: {
+    type: String,
+    default: 'USER',
+  },
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
