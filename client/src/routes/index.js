403b9b23fe25e14ffc5640a482aea6f311a9bd30
pierrolabo <pierrolabo@gmail.com>
2020-06-03 00:09:44 +0200

    done + login success redirect to home

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import NavBar from '../components/NavBar';
+import Home from '../views/Home';
 import Register from '../components/auth/Register';
+import Login from '../components/auth/Login';
 
 const routes = (
   <div>
+    <NavBar />
     <Switch>
-      <Route exact path='/' component={Register} />
+      <Route exact path='/' component={Home} />
+      <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} />
     </Switch>
   </div>
 );
