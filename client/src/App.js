59d727a5cfa52c80bc5b4a362a3b1997a3e2af6a
pierrolabo <pierrolabo@gmail.com>
2020-06-02 20:58:17 +0200

    Router is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-
+import { Route, Switch } from 'react-router';
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from './configureStore';
 import Register from './components/auth/Register';
 import { Provider } from 'react-redux';
-import store from './store';
+
+import routes from './routes';
 //import { loadUser } from './actions/authActions';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const store = configureStore({});
 class App extends Component {
   componentDidMount() {
     //store.dispatch(loadUser());
@@ -15,11 +19,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className='App'>
-          <Container>
-            <Register />
-          </Container>
-        </div>
+        <ConnectedRouter history={history}>{routes}</ConnectedRouter>
       </Provider>
     );
   }
---
+++ client/src/configureStore.js
@@ -0,0 +1,22 @@
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
+import thunk from 'redux-thunk';
+import createRootReducer from './reducers/reducers';
+
+export const history = createBrowserHistory();
+const middleware = [thunk];
+
+export default function configureStore(preloadedState) {
+  const store = createStore(
+    createRootReducer(history),
+    preloadedState,
+    compose(
+      applyMiddleware(routerMiddleware(history)),
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+  return store;
+}
