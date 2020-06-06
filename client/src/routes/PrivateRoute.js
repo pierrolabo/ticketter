687e40c6a73b0e5ad304212eb7de2706e6d36fae
pierrolabo <pierrolabo@gmail.com>
2020-06-06 23:57:47 +0200

    pending

--- client/src/routes/PrivateRoute.js
+++ client/src/routes/PrivateRoute.js
@@ -3,16 +3,15 @@ import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ children, auth, ...rest }) => {
   console.log('routeauth: ', auth);
-  /*
+
   return (
     <Route
       {...rest}
       render={(location) =>
-        auth.isAuthenticated ? <children /> : <Redirect to='/login' />
+        auth.isAuthenticated ? children : <Redirect to='/login' />
       }
     />
   );
-  */
 };
 
 export default PrivateRoute;
