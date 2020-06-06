9cbfb679fe699c6e5870376e1b666b2b0e15302f
pierrolabo <pierrolabo@gmail.com>
2020-06-07 00:17:08 +0200

    Persist store ok

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//  Redux && stuff
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import { Provider } from 'react-redux';
-import { AppContainer } from 'react-hot-loader';
 import configureStore, { history } from './configureStore';
+import { AppContainer } from 'react-hot-loader';
+
 const store = configureStore({});
+let persistor = persistStore(store);
 
 const render = () => {
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
-        <App history={history} />
+        <PersistGate loading={null} persistor={persistor}>
+          <App history={history} />
+        </PersistGate>
       </Provider>
     </AppContainer>,
     document.getElementById('root')
--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -3,13 +3,6 @@ import { connectRouter } from 'connected-react-router';
 import errorReducer from './errorReducer';
 import authReducer from './authReducer';
 
-/*
-export default combineReducers({
-  auth: authReducer,
-  error: errorReducer,
-});
-*/
-
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
