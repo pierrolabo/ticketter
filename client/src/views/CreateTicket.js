8fcd2c5e181508df9275ee1ecffd02b26072e3b0
bardamuffin <bardamuffin@gmail.com>
2020-06-17 12:26:32 +0200

    Added CardHeader

--- client/src/views/CreateTicket.js
+++ client/src/views/CreateTicket.js
@@ -14,6 +14,7 @@ import {
   Row,
   Col,
   Alert,
+  CardHeader,
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -121,6 +122,7 @@ class CreateTicket extends Component {
     return (
       <Container>
         <Card>
+          <CardHeader>Create a new ticket</CardHeader>
           <Form onSubmit={this.handleSubmit}>
             <FormGroup>
               <Label for='title'>Title</Label>
