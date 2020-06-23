ba6c5b434874dc8fc55431859952c015f13c7d42
bardamuffin <bardamuffin@gmail.com>
2020-06-23 16:06:20 +0200

    Fix Errors

--- client/src/routes/PrivateRoute.js
+++ client/src/routes/PrivateRoute.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ children, auth, ...rest }) => {
-  console.log('auth', auth);
   return (
     <Route
       {...rest}
